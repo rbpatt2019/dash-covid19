51ad6ab77675d05ffc8046a9cba532b37b20fedf
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-04-28 11:10:10 -0400

    Chained callback example

--- src/dash_covid19/app.py
+++ src/dash_covid19/app.py
@@ -8,34 +8,41 @@ external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+all_options = {
+    "America": ["New York City", "San Francisco", "Cincinnati"],
+    "Canada": [u"Montréal", "Toronto", "Ottawa"],
+}
 app.layout = html.Div(
     [
-        dcc.Input(id="num-multi", type="number", value=5),
-        html.Table(
-            [
-                html.Tr([html.Td(["x", html.Sup(2)]), html.Td(id="square")]),
-                html.Tr([html.Td(["x", html.Sup(3)]), html.Td(id="cube")]),
-                html.Tr([html.Td([2, html.Sup("x")]), html.Td(id="twos")]),
-                html.Tr([html.Td([3, html.Sup("x")]), html.Td(id="threes")]),
-                html.Tr([html.Td(["x", html.Sup("x")]), html.Td(id="x^x")]),
-            ]
+        dcc.RadioItems(
+            id="countries-radio",
+            options=[{"label": k, "value": k} for k in all_options.keys()],
+            value="America",
         ),
+        html.Hr(),
+        dcc.RadioItems(id="cities-radio"),
+        html.Hr(),
+        html.Div(id="display-selected-values"),
     ]
 )
 
 
+@app.callback(Output("cities-radio", "options"), [Input("countries-radio", "value")])
+def set_cities_options(selected_country):
+    return [{"label": i, "value": i} for i in all_options[selected_country]]
+
+
+@app.callback(Output("cities-radio", "value"), [Input("cities-radio", "options")])
+def set_cities_value(available_options):
+    return available_options[0]["value"]
+
+
 @app.callback(
-    [
-        Output("square", "children"),
-        Output("cube", "children"),
-        Output("twos", "children"),
-        Output("threes", "children"),
-        Output("x^x", "children"),
-    ],
-    [Input("num-multi", "value")],
+    Output("display-selected-values", "children"),
+    [Input("countries-radio", "value"), Input("cities-radio", "value")],
 )
-def callback_a(x):
-    return x ** 2, x ** 3, 2 ** x, 3 ** x, x ** x
+def set_display_children(selected_country, selected_city):
+    return u"{} is a city in {}".format(selected_city, selected_country,)
 
 
 if __name__ == "__main__":
