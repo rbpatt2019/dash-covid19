1530259bbf210ed5d0f30f9c8a5c66335b732f3b
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-04-28 11:06:36 -0400

    Files altered

--- src/dash_covid19/app.py
+++ src/dash_covid19/app.py
@@ -2,118 +2,40 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
 from dash.dependencies import Input, Output
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-df = pd.read_csv("https://plotly.github.io/datasets/country_indicators.csv")
-
-available_indicators = df["Indicator Name"].unique()
-
 app.layout = html.Div(
     [
-        html.Div(
+        dcc.Input(id="num-multi", type="number", value=5),
+        html.Table(
             [
-                html.Div(
-                    [
-                        dcc.Dropdown(
-                            id="xaxis-column",
-                            options=[
-                                {"label": i, "value": i} for i in available_indicators
-                            ],
-                            value="Fertility rate, total (births per woman)",
-                        ),
-                        dcc.RadioItems(
-                            id="xaxis-type",
-                            options=[
-                                {"label": i, "value": i} for i in ["Linear", "Log"]
-                            ],
-                            value="Linear",
-                            labelStyle={"display": "inline-block"},
-                        ),
-                    ],
-                    style={"width": "48%", "display": "inline-block"},
-                ),
-                html.Div(
-                    [
-                        dcc.Dropdown(
-                            id="yaxis-column",
-                            options=[
-                                {"label": i, "value": i} for i in available_indicators
-                            ],
-                            value="Life expectancy at birth, total (years)",
-                        ),
-                        dcc.RadioItems(
-                            id="yaxis-type",
-                            options=[
-                                {"label": i, "value": i} for i in ["Linear", "Log"]
-                            ],
-                            value="Linear",
-                            labelStyle={"display": "inline-block"},
-                        ),
-                    ],
-                    style={"width": "48%", "float": "right", "display": "inline-block"},
-                ),
+                html.Tr([html.Td(["x", html.Sup(2)]), html.Td(id="square")]),
+                html.Tr([html.Td(["x", html.Sup(3)]), html.Td(id="cube")]),
+                html.Tr([html.Td([2, html.Sup("x")]), html.Td(id="twos")]),
+                html.Tr([html.Td([3, html.Sup("x")]), html.Td(id="threes")]),
+                html.Tr([html.Td(["x", html.Sup("x")]), html.Td(id="x^x")]),
             ]
-        ),
-        dcc.Graph(id="indicator-graphic"),
-        dcc.Slider(
-            id="year--slider",
-            min=df["Year"].min(),
-            max=df["Year"].max(),
-            value=df["Year"].max(),
-            marks={str(year): str(year) for year in df["Year"].unique()},
-            step=None,
         ),
     ]
 )
 
 
 @app.callback(
-    Output("indicator-graphic", "figure"),
     [
-        Input("xaxis-column", "value"),
-        Input("yaxis-column", "value"),
-        Input("xaxis-type", "value"),
-        Input("yaxis-type", "value"),
-        Input("year--slider", "value"),
+        Output("square", "children"),
+        Output("cube", "children"),
+        Output("twos", "children"),
+        Output("threes", "children"),
+        Output("x^x", "children"),
     ],
+    [Input("num-multi", "value")],
 )
-def update_graph(
-    xaxis_column_name, yaxis_column_name, xaxis_type, yaxis_type, year_value
-):
-    dff = df[df["Year"] == year_value]
-
-    return {
-        "data": [
-            dict(
-                x=dff[dff["Indicator Name"] == xaxis_column_name]["Value"],
-                y=dff[dff["Indicator Name"] == yaxis_column_name]["Value"],
-                text=dff[dff["Indicator Name"] == yaxis_column_name]["Country Name"],
-                mode="markers",
-                marker={
-                    "size": 15,
-                    "opacity": 0.5,
-                    "line": {"width": 0.5, "color": "white"},
-                },
-            )
-        ],
-        "layout": dict(
-            xaxis={
-                "title": xaxis_column_name,
-                "type": "linear" if xaxis_type == "Linear" else "log",
-            },
-            yaxis={
-                "title": yaxis_column_name,
-                "type": "linear" if yaxis_type == "Linear" else "log",
-            },
-            margin={"l": 40, "b": 40, "t": 10, "r": 0},
-            hovermode="closest",
-        ),
-    }
+def callback_a(x):
+    return x ** 2, x ** 3, 2 ** x, 3 ** x, x ** x
 
 
 if __name__ == "__main__":
