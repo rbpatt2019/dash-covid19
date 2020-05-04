94ac17aa8fe074b893826646c297c2fbb3e39d89
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-05-04 11:28:15 -0400

    Remove unused imports

--- dash_covid19/app.py
+++ dash_covid19/app.py
@@ -2,8 +2,6 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
-from dash.dependencies import Input, Output
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
