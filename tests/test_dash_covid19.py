c8e1a324a491ed69f37970fe854749b5189702a7
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-05-07 12:04:21 -0400

    Working test suite

--- tests/test_dash_covid19.py
+++ tests/test_dash_covid19.py
@@ -1,5 +1,24 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_html_components as html
+from dash.testing.application_runners import import_app
 from dash_covid19 import __version__
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_sample(dash_duo):
+    app = import_app("dash_covid19.app")
+
+    dash_duo.start_server(app)
+
+    # Wait for button to initialise
+    btn = dash_duo.wait_for_element_by_css_selector("#click-me")
+
+    # Click it and check output
+    btn.click()
+    dash_duo.wait_for_text_to_equal("#out", "Clicked: 1")
+
+    # Should increment when clicked multiple times
+    btn.click()
+    dash_duo.wait_for_text_to_equal("#out", "Clicked: 2")
+
+    assert dash_duo.get_logs() == [], "browser console should contain no error"
