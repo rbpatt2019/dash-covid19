e6a152d86ca534939d87c13ca8b399df7ded9526
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-05-10 11:47:06 -0400

    Update test to include percy

--- tests/test_dash_covid19.py
+++ tests/test_dash_covid19.py
@@ -5,7 +5,7 @@ from dash.testing.application_runners import import_app
 from dash_covid19 import __version__
 
 
-def test_sample(dash_duo):
+def test_dcap001_btn_click(dash_duo):
     app = import_app("dash_covid19.app")
 
     dash_duo.start_server(app)
@@ -22,3 +22,4 @@ def test_sample(dash_duo):
     dash_duo.wait_for_text_to_equal("#out", "Clicked: 2")
 
     assert dash_duo.get_logs() == [], "browser console should contain no error"
+    dash_duo.percy_snapshot("dcap001-layout")
