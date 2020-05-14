6b089e69d1843e9db012bcac511b7a71bfdb2430
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-05-14 06:17:13 -0400

    Remove currently unnecessary callback imports

--- dash_covid19/index.py
+++ dash_covid19/index.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import dash_core_components as dcc
-import dash_covid19.callbacks
 import dash_html_components as html
 from dash.dependencies import Input, Output
 from dash_covid19.app import app
