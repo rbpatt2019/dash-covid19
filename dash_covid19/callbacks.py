ce8d953d185603613cc5803147a35cce470605cd
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-05-15 04:24:32 -0400

    Create init_callbacks

--- dash_covid19/callbacks.py
+++ dash_covid19/callbacks.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from dash.dependencies import Input, Output
 
-from dash_covid19.layouts import layouts
 
-
-@app.callback(Output("page-content", "children"), [Input("navigation", "value")])
-def display_page(value):
-    return layouts[value]
+def init_callbacks(dash_app, layouts):
+    @dash_app.callback(
+        Output("page-content", "children"), [Input("navigation", "value")]
+    )
+    def display_page(value):
+        return layouts[value]
