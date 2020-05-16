315fbf57b8b4a5001eb1e5681d1fe9b97a28f36e
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-05-16 11:06:22 -0400

    Switch to wait for element by id

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -9,10 +9,10 @@ def test_dccb001_tab_navigation(dash_duo):
     dash_duo.start_server(app)
 
     # Check defaults to explorer
-    dash_duo.wait_for_element("exp-world-map")
+    dash_duo.wait_for_element_by_id("exp-world-map")
     dash_duo.percy_snapshot("dcin001-graph")
 
     # # Check change correctly
     dash_duo.click_at_coord_fractions("#nav-tab-2", 0.5, 0.5)
-    dash_duo.wait_for_element("#dt-data")
+    dash_duo.wait_for_element_by_id("dt-data")
     dash_duo.percy_snapshot("dcin001-data-table")
