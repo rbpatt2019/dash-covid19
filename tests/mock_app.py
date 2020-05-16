11be7067c8dea699f5b06b9ed6cb89b968e2cf63
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-05-16 11:10:04 -0400

    Update mock app to create data with date column, as directly referenced in layout

--- tests/mock_app.py
+++ tests/mock_app.py
@@ -5,8 +5,8 @@ from dash import Dash
 app = Dash(__name__)
 
 
-def mock_data(nrows=2, ncols=2):
+def mock_data():
     """Create a mock dataframe for testing"""
-    data = {str(i): range(nrows) for i in range(ncols)}
+    data = {"date": ["2020-03-01", "2020-03-02"], "number": [1, 2]}
     df = pd.DataFrame(data=data)
     return df
