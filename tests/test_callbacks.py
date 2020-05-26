eecf8bfe7d1dbab9d4ac8b13f1237b0097c77ca0
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-05-26 06:42:18 -0400

    Minimal working dropdown test

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -2,6 +2,8 @@
 import dash
 from dash.testing.application_runners import import_app
 
+from dash_covid19 import columns
+
 
 def test_dccb001_tab_navigation(dash_duo):
     app = import_app("wsgi")
@@ -16,3 +18,12 @@ def test_dccb001_tab_navigation(dash_duo):
     dash_duo.click_at_coord_fractions("#nav-tab-2", 0.5, 0.5)
     dash_duo.wait_for_element_by_id("dt-data")
     dash_duo.percy_snapshot("dcin001-data-table")
+
+
+def test_dccb002_dropdown(dash_duo):
+    app = import_app("wsgi")
+
+    dash_duo.start_server(app)
+
+    dd = dash_duo.wait_for_element_by_id("exp-dd-column")
+    assert columns[0] in dd.text
