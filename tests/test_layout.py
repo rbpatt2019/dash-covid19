fa84b446f4f4f52eeb2431062d92a29f7a40c74b
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-06-17 12:14:49 -0400

    Update init_layout call

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -2,14 +2,14 @@
 import dash
 from dash.testing.application_runners import import_app
 
-from dash_covid19 import data, columns
+from dash_covid19 import data, columns, date_idx
 from dash_covid19.layouts import init_layouts
 
 
 def test_dcly001_init_layout(dash_duo):
     app = dash.Dash(__name__)
 
-    app, layouts = init_layouts(app, data, columns)
+    app, layouts = init_layouts(app, data, columns, date_idx)
 
     assert app.layout == layouts["app"]
     assert type(app) == dash.dash.Dash
