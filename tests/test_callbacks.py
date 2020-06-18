2760f08a16340d0167c8ea36eaf21295204731d8
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-06-18 07:14:55 -0400

    Correc test to avoid travis fail

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -26,12 +26,12 @@ def test_dccb001_tab_navigation(dash_duo):
     # Check chage to graph
     dash_duo.click_at_coord_fractions("#nav-bar-exp", 0.5, 0.5)
     dash_duo.wait_for_element_by_id("exp-main-scatter")
-    dash_duo.percy_snapshot("dccb001-graph", wait_for_callbacks=True)
+    dash_duo.percy_snapshot("dccb001-graph")
 
     # Change to date table...
     dash_duo.click_at_coord_fractions("#nav-bar-dt", 0.5, 0.5)
     dash_duo.wait_for_element_by_id("dt-data")
-    dash_duo.percy_snapshot("dccb001-data-table", wait_for_callbacks=True)
+    dash_duo.percy_snapshot("dccb001-data-table")
 
 
 def test_dccb002_interactive_graph(dash_duo):
@@ -43,14 +43,14 @@ def test_dccb002_interactive_graph(dash_duo):
     # Wait for graph to prevent image "jumping"
     dash_duo.wait_for_element_by_id("nav-bar-exp")
     dash_duo.click_at_coord_fractions("#nav-bar-exp", 0.5, 0.5)
-    dash_duo.wait_for_element_by_id("exp-main-scatter")
+    sleep(3)
 
     # Interact with all the things!
-    dash_duo.select_dcc_dropdown("#exp-dd-x", index=3)
-    dash_duo.select_dcc_dropdown("#exp-dd-y", index=9)
     dash_duo.click_at_coord_fractions("#exp-scale-x", 0.5, 0.5)
     dash_duo.click_at_coord_fractions("#exp-scale-y", 0.5, 0.5)
     dash_duo.click_at_coord_fractions("#exp-main-slider", 0.5, 0.1)
+    dash_duo.select_dcc_dropdown("#exp-dd-x", index=3)
+    dash_duo.select_dcc_dropdown("#exp-dd-y", index=9)
 
     # And confirm
     dash_duo.percy_snapshot("dccb002-graph", wait_for_callbacks=True)
