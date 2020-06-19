3abf7a18853a9ab4072a084f59130a57c777f0a5
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-06-19 10:00:17 -0400

    Use mock data

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -6,10 +6,15 @@ from dash_covid19 import data, columns, date_idx
 from dash_covid19.layouts import init_layouts
 
 
-def test_dcly001_init_layout(dash_duo):
+def test_dcly001_init_layout(mimesis_data):
     app = dash.Dash(__name__)
 
-    app, layouts = init_layouts(app, data, columns, date_idx)
+    app, layouts = init_layouts(
+        app,
+        mimesis_data,
+        mimesis_data.columns[4:],
+        range(len(mimesis_data["date"].unique())),
+    )
 
     assert app.layout == layouts["app"]
     assert type(app) == dash.dash.Dash
