85f07ea10ecc4786d7f98958dd65f1452e885b61
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-06-19 10:37:41 -0400

    Re-enable headless mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,7 @@ from dash_covid19 import create_app
 
 def pytest_setup_options():
     options = Options()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
